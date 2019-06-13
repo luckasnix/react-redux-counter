7491e0dd19b30d5fafdbb13d4c9ec2a37e8e280e
luckasnix <luc.kasnix@gmail.com>
2019-06-13 14:04:13 -0300

    Início do projeto

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('# APP',() => {
+    it('Test 1', () => {
+        expect(1).toBe(2);
+    });
 });
